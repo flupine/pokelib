484e5dc056c41f165f96c51b77b2620d133cf001
Faurest LUPINE <faurest@blockpulse.eu>
2022-03-14 21:13:44 +0100

    Fix empty /monster page

---
+++ pages/monster/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Nothing here...
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'MonsterIndex',
+  beforeMount(): void {
+      this.$nuxt.$options.router!.push('/');
+  },
+})
+</script>
