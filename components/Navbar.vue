b96285fde60004f25330246785475d3abb44c7b7
Faurest LUPINE <faurest@blockpulse.eu>
2022-03-14 19:09:09 +0100

    Fix navbar links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -57,26 +57,14 @@
       </div>
       <div :class="[isOpen ? '' : 'hidden', 'md:hidden']">
         <div class="px-2 pt-2 pb-3 sm:px-3">
-          <a
-              href="#"
+          <NuxtLink
+              to="/"
               class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-          >Dashboard</a>
-          <a
-              href="#"
+          >Accueil</NuxtLink>
+          <button
+              @click="showModal = true"
               class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-          >Team</a>
-          <a
-              href="#"
-              class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-          >Projects</a>
-          <a
-              href="#"
-              class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-          >Calendar</a>
-          <a
-              href="#"
-              class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-          >Reports</a>
+          >Team</button>
         </div>
       </div>
     </nav>
